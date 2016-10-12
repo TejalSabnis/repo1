ae770b091722e0486109d116b19b079f1b956d11
TejalSabnis <tejal.sabnis@gmail.com>
2016-10-12 14:40:38 -0700

    second

--- Assignment2_Interface.py
+++ Assignment2_Interface.py
@@ -15,26 +15,45 @@ RROBIN_TABLE_PREFIX = 'roundrobinratingspart'
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection):
     cur = openconnection.cursor();
+    cur2 = openconnection.cursor();
     text_file = open("RangeQueryOut.txt", "w")
-    # print("fetching metadata")
+    # fetching from range partitions
+    cur.execute("select partitionnum,minrating,maxrating from rangeratingsmetadata;");
+    for (partitionnum,minrating,maxrating) in cur:
+        if ratingMaxValue>=minrating or ratingMinValue<=maxrating:
+            cur2.execute("select userid,movieid,rating from "+RANGE_TABLE_PREFIX+str(partitionnum)+" where rating>=%s and rating<=%s",(ratingMinValue,ratingMaxValue));
+            for (userid,movieid,rating) in cur2:
+                text_file.write(RANGE_TABLE_PREFIX+str(partitionnum)+","+str(userid)+","+str(movieid)+","+str(rating)+"\n");
+    # fetching from round robin partitions
     cur.execute("select partitionnum from roundrobinratingsmetadata;");
     partitions = int(cur.fetchone()[0])
     for i in range(0,partitions):
         cur.execute("select userid,movieid,rating from "+RROBIN_TABLE_PREFIX+str(i)+" where rating>=%s and rating<=%s",(ratingMinValue,ratingMaxValue));
         for (userid,movieid,rating) in cur:
-            print userid,movieid,rating;
             text_file.write(RROBIN_TABLE_PREFIX+str(i)+","+str(userid)+","+str(movieid)+","+str(rating)+"\n");
     text_file.close();
 
 def PointQuery(ratingsTableName, ratingValue, openconnection):
     cur = openconnection.cursor();
+    cur2 = openconnection.cursor();
     text_file = open("PointQueryOut.txt", "w")
-    # print("fetching metadata")
+    # fetching from range partitions
+    cur.execute("select partitionnum,minrating,maxrating from rangeratingsmetadata;");
+    for (partitionnum,minrating,maxrating) in cur:
+        if partitionnum == 0 and ratingValue>=minrating and ratingValue<=maxrating:
+            cur2.execute("select userid,movieid,rating from "+RANGE_TABLE_PREFIX+str(partitionnum)+" where rating=%s",(str(ratingValue),));
+            for (userid,movieid,rating) in cur2:
+                text_file.write(RANGE_TABLE_PREFIX+str(partitionnum)+","+str(userid)+","+str(movieid)+","+str(rating)+"\n");
+        else:
+            if ratingValue>minrating and ratingValue<=maxrating:
+                cur2.execute("select userid,movieid,rating from "+RANGE_TABLE_PREFIX+str(partitionnum)+" where rating=%s",(str(ratingValue),));
+                for (userid,movieid,rating) in cur2:
+                    text_file.write(RANGE_TABLE_PREFIX+str(partitionnum)+","+str(userid)+","+str(movieid)+","+str(rating)+"\n");
+    # fetching from round robin partitions
     cur.execute("select partitionnum from roundrobinratingsmetadata;");
     partitions = int(cur.fetchone()[0])
     for i in range(0,partitions):
         cur.execute("select userid,movieid,rating from "+RROBIN_TABLE_PREFIX+str(i)+" where rating=%s",(str(ratingValue),));
         for (userid,movieid,rating) in cur:
-            print userid,movieid,rating;
             text_file.write(RROBIN_TABLE_PREFIX+str(i)+","+str(userid)+","+str(movieid)+","+str(rating)+"\n");
     text_file.close();
